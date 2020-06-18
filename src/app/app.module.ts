2ab684e97cb41bb62d79bf8bed2fa1156f862371
Luis Fernandez <fernandezluisn@gmail.com>
2020-06-19 00:37:22 +0200

    primer post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RutasRoutingModule} from  './rutas/rutas-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RutasRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
