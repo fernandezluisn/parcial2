e4025803d1b292068a524f60628828a1038291fd
Luis Fernandez <fernandezluisn@gmail.com>
2020-06-25 21:16:48 +0200

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCW_OVRn3U3HpQAOCufwtgttEMTat1xFGI",
+    authDomain: "parcial2lnf.firebaseapp.com",
+    databaseURL: "https://parcial2lnf.firebaseio.com",
+    projectId: "parcial2lnf",
+    storageBucket: "parcial2lnf.appspot.com",
+    messagingSenderId: "23816856346",
+    appId: "1:23816856346:web:a906401e841f2b2f33c2a3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCW_OVRn3U3HpQAOCufwtgttEMTat1xFGI",
+    authDomain: "parcial2lnf.firebaseapp.com",
+    databaseURL: "https://parcial2lnf.firebaseio.com",
+    projectId: "parcial2lnf",
+    storageBucket: "parcial2lnf.appspot.com",
+    messagingSenderId: "23816856346",
+    appId: "1:23816856346:web:a906401e841f2b2f33c2a3"
+  }
 };
 
 /*
